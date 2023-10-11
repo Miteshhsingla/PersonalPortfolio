6b953142c5a286447cbe9c09c1c2d3bfacb9e558
miteshsingla <miteshhsingla@gmail.com>
2023-10-11 09:56:39 +0530

    minor changes#3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { useRef } from 'react';
 import './index.css';
 
-import img from './bitmoji.svg';
-import img2 from './HeroImg.svg';
-import bottomImg from './bottomDesign.svg';
-import bottomArrow from './bottomArrow.svg';
-import github from './socialMediaIcons/ri_github-fill.svg';
-import instagram from './socialMediaIcons/ri_instagram-line.svg';
-import linkedin from './socialMediaIcons/ri_linkedin-fill.svg';
-import twitter from './socialMediaIcons/ri_twitter-fill.svg';
-import Cursor from "./cursor"
-
+import img from './assets/bitmoji.svg';
+import img2 from './assets/HeroImg.svg';
+import bottomImg from './assets/bottomDesign.svg';
+import bottomArrow from './assets/bottomArrow.svg';
+import github from './assets/socialMediaIcons/ri_github-fill.svg';
+import instagram from './assets/socialMediaIcons/ri_instagram-line.svg';
+import linkedin from './assets/socialMediaIcons/ri_linkedin-fill.svg';
+import twitter from './assets/socialMediaIcons/ri_twitter-fill.svg';
+import Cursor from "./components/cursor";
+import AboutMeBg from "./assets/AboutMeBg.svg";
+import cup from "./assets/cup.svg";
 
 
 const Navbar = () => {
@@ -64,9 +65,9 @@ const SocialMedia = () => {
 
 const App = () => {
   const aboutRef = useRef(null);
-  const projectsRef = useRef(null);
-  const resumeRef = useRef(null);
-  const contactRef = useRef(null);
+  // const projectsRef = useRef(null);
+  // const resumeRef = useRef(null);
+  // const contactRef = useRef(null);
 
   const scrollToSection = (ref) => {
     ref.current.scrollIntoView({ behavior: 'smooth' });
@@ -81,12 +82,19 @@ const App = () => {
         <SocialMedia />
       </div>
 
-      <section id="about-section" className="section" ref={aboutRef}>
+
+      <section id="about-section" className="Aboutsection" ref={aboutRef}>
         <div className="aboutsection">
-          <p className="name">ABOUT ME</p>
-          <p className='aboutMeDescription'>I am Mitesh Singla, someone who’s always been interested in design and development. I believe for most of the products, both of these go hand in hand.Because of this enthusiasm towards tech & UX I have come across wide variety of technologies and projects, which I try to update here on this website.</p>
+          <div className='bgImage'>
+            <img src={AboutMeBg} alt="icon" className="bg-image" />
+          </div>
+          <p className="AboutTitle">ABOUT ME</p>
+          <p className='aboutMeDescription'>I'm<span style={{ color: 'rgba(195, 190, 171, 1)' }}> Mitesh Singla</span>, someone who’s always been interested in design and development. I believe for most of the products, both of these go hand in hand.Because of this enthusiasm towards tech & UX I have come across wide variety of technologies and projects, which I try to update here on this website.</p>
         </div>
       </section>
+      <div>
+        <img src={cup} alt="SVG" className="svg-image" />
+      </div>
     </div>
 
   );
