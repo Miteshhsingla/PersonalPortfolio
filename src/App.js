6b974ff26d8a28d51669a423e1eb49d68219449d
vaibhavsharma <sharmavishu052@gmail.com>
2023-10-11 09:25:26 +0530

    latest

---
+++ src/App.js
@@ -0,0 +1,99 @@
+import React, { useRef } from 'react';
+import './index.css';
+
+import img from './bitmoji.svg';
+import img2 from './HeroImg.svg';
+import bottomImg from './bottomDesign.svg';
+import bottomArrow from './bottomArrow.svg';
+import github from './socialMediaIcons/ri_github-fill.svg';
+import instagram from './socialMediaIcons/ri_instagram-line.svg';
+import linkedin from './socialMediaIcons/ri_linkedin-fill.svg';
+import twitter from './socialMediaIcons/ri_twitter-fill.svg';
+import Cursor from "./cursor"
+
+
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <ul>
+        <li><a href="#about" >ABOUT</a></li>
+        <li><a href="#projects">PROJECTS</a></li>
+        <li><a href="#resume">RESUME</a></li>
+        <li><a href="#contact">CONTACT</a></li>
+      </ul>
+    </div>
+  );
+};
+
+const HeroSection = ({ scrollToAbout }) => {
+  return (
+    <div className="hero-section">
+      <div className="content">
+        {/* <img src={img} alt="icon" className="icon-image" /> */}
+        <p className="name">MITESH SINGLA</p>
+        <p className='heading' >Crafting <span style={{ color: 'rgba(195, 190, 171, 1)' }}>Imagination</span> into Digital Experiences.</p>
+       
+      </div>
+      <div  className="hero_img">
+        <img src={img2} alt="MainImg" />
+      </div>
+      <div className='div-design'>
+        <img src={bottomImg} alt="design" />
+      </div>
+      <div>
+        <img src={bottomArrow} alt="arrow" className="bottom-arrow" onClick={scrollToAbout} />
+      </div>
+       <SocialMedia />
+     
+    </div>
+  );
+};
+
+const SocialMedia = () => {
+  return (
+    <div className="social-media">
+      <ul>
+        <li><img src={github} alt="icon" className="social-image" /></li>
+        <li><img src={instagram} alt="icon" className="social-image" /></li>
+        <li><img src={linkedin} alt="icon" className="social-image" /></li>
+        <li><img src={twitter} alt="icon" className="social-image" /></li>
+      </ul>
+    </div>
+  );
+};
+
+
+
+const App = () => {
+  const aboutRef = useRef(null);
+  const projectsRef = useRef(null);
+  const resumeRef = useRef(null);
+  const contactRef = useRef(null);
+
+  const scrollToSection = (ref) => {
+    ref.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  return (
+    <div>
+      <div className="container">
+        <Cursor />
+        <Navbar />
+        <HeroSection scrollToAbout={() => scrollToSection(aboutRef)} />
+      
+      </div>
+
+      <section id="about-section" className="section" ref={aboutRef}>
+        <div className="aboutsection">
+          <p className="name">ABOUT ME</p>
+          <p className='aboutMeDescription'>I am Mitesh Singla, someone who’s always been interested in design and development. I believe for most of the products, both of these go hand in hand.Because of this enthusiasm towards tech & UX I have come across wide variety of technologies and projects, which I try to update here on this website.</p>
+        </div>
+      </section>
+    </div>
+
+  );
+};
+
+
+export default App;
