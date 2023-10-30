4078988acaa043692b3c29008edb751f16d4143f
miteshsingla <miteshhsingla@gmail.com>
2023-10-30 18:49:08 +0530

    contact updated

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import img from '../assets/bitmoji.svg';
+import cross from '../assets/cross.svg';
 
 const Contact = () => {
     const [isOpen, setIsOpen] = useState(true);
@@ -9,22 +10,23 @@ const Contact = () => {
     const options = ['Say Hi', 'Download Resume', 'Contact Me'];
 
     useEffect(() => {
-
         if (isOpen && !displayHi) {
-            // Simulate a system message arriving ("Hey, What's up?") with a delay
+            // Simulate a system message arriving with a delay
             setTimeout(() => {
                 setChatMessages([
                     ...chatMessages,
-                    { text: 'Hey, What\'s up?', isSystem: true },
+                    {
+                        text: "Hey👋🏽, What's up?",
+                        isSystem: true,
+                    }, {
+                        text: "I'm Mitesh, and I spend the majority of my time engaged in the design and development of various projects.",
+                        isSystem: true,
+                    }
                 ]);
                 setDisplayHi(true);
             }, 1000);
         }
     }, [isOpen, chatMessages, displayHi]);
-
-    // const toggleChat = () => {
-    //     ;
-    // };
 
     const handleOptionClick = (option) => {
         if (option === 'Say Hi') {
@@ -32,14 +34,22 @@ const Contact = () => {
             setChatMessages([
                 ...chatMessages,
                 { text: 'Hi', isUser: true },
+                { text: 'Hello', isSystem: true },
+                { text: 'I hope you enjoyed surfing my portfolio.', isSystem: true },
+                { text: 'Can i help you with anything else?', isSystem: true }
+
             ]);
         } else if (option === 'Contact Me') {
             // Display "Let's connect" message and provide your email
             setChatMessages([
                 ...chatMessages,
+                { text: "Contact Me", isUser: true },
                 { text: "Let's connect", isSystem: true },
                 { text: 'Email: abc@gmail.com', isSystem: true },
             ]);
+        } else if (option === 'Close') {
+            // Close the chat interface
+            setIsOpen(false);
         } else {
             // For other options, just display the selected option as a user message
             setChatMessages([...chatMessages, { text: option, isUser: true }]);
@@ -50,7 +60,6 @@ const Contact = () => {
 
     return (
         <div className="chat-interface">
-
             {isOpen && (
                 <div className="chat-box">
                     <div className="chat-header">
@@ -60,11 +69,14 @@ const Contact = () => {
                                 alt="Your Name"
                                 className="user-photo"
                             />
-                            <h2>Mitesh Singla</h2>
-                            <p>Ask me a question</p>
+                            <text>Mitesh Singla</text>
                         </div>
-                        <span className="close-button" >
-                            X
+                        <span className="close-button" onClick={() => handleOptionClick('Close')}>
+                            <img
+                                src={cross}
+                                alt="Back"
+                                className="back"
+                            />
                         </span>
                     </div>
                     <div className="chat-messages">
