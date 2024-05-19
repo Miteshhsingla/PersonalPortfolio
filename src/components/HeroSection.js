38671848585f41c7ccb8f1a8554505d2142e5336
miteshsingla <miteshhsingla@gmail.com>
2024-05-19 17:35:44 +0530

    nav

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -19,14 +19,12 @@ const HeroSection = ({ scrollToAbout }) => {
 
         <div className="hero-section">
 
-
             <div className="content">
                 <p className="name">MITESH SINGLA</p>
                 <p className='heading' >CRAFTING <span style={{ color: '#BB9CFF' }}>IMAGINATION</span> INTO DIGITAL EXPERIENCES</p>
             </div>
-            {/* <div className="hero_img">
-                <img src={img2} alt="MainImg" />
-            </div> */}
+
+
             <div className='div-design'>
                 <img src={bottomImg} alt="design" />
             </div>
